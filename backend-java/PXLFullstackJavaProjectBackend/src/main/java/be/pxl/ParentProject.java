7f474a2118c41b59c00801743315441e676f8429
WouterJacobsPXL <wouter.jacobs@student.pxl.be>
2024-11-14 13:53:57 +0100

    [Backend] : Init of ParentProject

---
+++ backend-java/PXLFullstackJavaProjectBackend/src/main/java/be/pxl/ParentProject.java
@@ -0,0 +1,12 @@
+package be.pxl;
+
+/**
+ * ParentProject
+ */
+public class ParentProject
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Parent project" );
+    }
+}
