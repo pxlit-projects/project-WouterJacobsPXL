6e8d7bce67af7eb3d325839d793b7ec7927a1a78
WouterJacobsPXL <wouter.jacobs@student.pxl.be>
2024-11-17 14:49:15 +0100

    [Backend] : Added new functions in service, exception + small change
    - Added a getAll and findById in PostService
    - Added a PostNotFoundException
    - Added a Title field in the Post class

--- backend-java/PXLFullstackJavaProjectBackend/Post-Service/src/main/java/be/pxl/domain/Post.java
+++ backend-java/PXLFullstackJavaProjectBackend/Post-Service/src/main/java/be/pxl/domain/Post.java
@@ -18,6 +18,10 @@ public class Post {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @NotBlank(message = "Title is required.")
+    @Size(min = 10, max = 100, message = "Title must be between 10 and 100 characters.")
+    private String title;
+
     @Lob
     @NotBlank(message = "Content is required.")
     @Size(min = 100, max = 50000, message = "Content must be between 100 and 50,000 characters.")
--- backend-java/PXLFullstackJavaProjectBackend/Post-Service/src/main/java/be/pxl/service/PostService.java
+++ backend-java/PXLFullstackJavaProjectBackend/Post-Service/src/main/java/be/pxl/service/PostService.java
@@ -1,10 +1,12 @@
 package be.pxl.service;
 
 import be.pxl.api.dto.PostRequestDto;
+import be.pxl.api.dto.PostResponseDto;
 import be.pxl.domain.Author;
 import be.pxl.domain.Post;
 import be.pxl.exception.AuthorNotFoundException;
 import be.pxl.exception.InvalidPostException;
+import be.pxl.exception.PostNotFoundException;
 import be.pxl.repository.AuthorRepository;
 import be.pxl.repository.PostRepository;
 import be.pxl.utils.PostMapper;
@@ -13,7 +15,10 @@ import jakarta.validation.Validator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
@@ -21,7 +26,7 @@ public class PostService {
     private final PostRepository postRepository;
     private final AuthorRepository authorRepository;
 
-    private void addPost(PostRequestDto postRequestDto){
+    public void addPost(PostRequestDto postRequestDto){
         //TODO add logging
         Author author = findAuthorById(postRequestDto.getAuthorId());
         Post post = PostMapper.toEntity(postRequestDto, author);
@@ -31,13 +36,25 @@ public class PostService {
         postRepository.save(post);
     }
 
+    public List<PostResponseDto> getPosts(){
+        //TODO add logging
+        return postRepository.findAll().stream().map(PostMapper::toResponse).toList();
+    }
+
+    public PostResponseDto getPostById(Long id){
+        //TODO add logging
+        return postRepository.findAll().stream()
+                .filter(post -> post.getId().equals(id)).map(PostMapper::toResponse)
+                .findFirst().orElseThrow(() -> new PostNotFoundException("Post with id: %d not found".formatted(id)));
+    }
+
     private Author findAuthorById(long id){
         return authorRepository.findById(id)
                 .orElseThrow(() -> new AuthorNotFoundException("Author with id: %d not found".formatted(id)) );
     }
 
     private void validatePost(Post post) {
-        //TODO add logging
+        //TODO add logging if unsuccessful
         Set<ConstraintViolation<Post>> violations = validator.validate(post);
         if (!violations.isEmpty()) {
             StringBuilder errorMessage = new StringBuilder("Validation errors:\n");
