f5d27e174ef994358db23a2b9bdae0cbf6760dbc
WouterJacobsPXL <wouter.jacobs@student.pxl.be>
2024-11-18 21:58:48 +0100

    [Frontend] Added a routeguard to check if you are the right user before entering certain pages

--- frontend-web/PXLFullstackJavaProjectFrontend/src/app/app.routes.ts
+++ frontend-web/PXLFullstackJavaProjectFrontend/src/app/app.routes.ts
@@ -3,9 +3,10 @@ import {PostcardListComponent} from "./posts/postcard-list/postcard-list.compone
 import {PostComponent} from "./posts/post/post.component";
 import {AddPostComponent} from "./posts/add-post/add-post.component";
 import {LoginComponent} from "./login/login.component";
+import {authenticationGuard} from "./routeGuards/authentication.guard";
 
 export const routes: Routes = [
-  { path: 'posts/new', component: AddPostComponent },
+  { path: 'posts/new', component: AddPostComponent, canActivate: [authenticationGuard], data: { roles: ['author'] }},
   { path: 'login', component: LoginComponent },
   { path: 'posts', component: PostcardListComponent },
   { path: 'posts/:id', component: PostComponent },
---
+++ frontend-web/PXLFullstackJavaProjectFrontend/src/app/routeGuards/authentication.guard.ts
@@ -0,0 +1,32 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+import {MatSnackBar} from "@angular/material/snack-bar";
+
+export const authenticationGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const snackBar = inject(MatSnackBar);
+
+  const userRole = localStorage.getItem('userRole');
+
+  if (!userRole) {
+    snackBar.open('Please log in to access this page', 'Close', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    });
+    router.navigate(['/login']);
+    return false;
+  }
+
+    if (userRole != "Author") {
+      snackBar.open(`Only authors can access this page`, 'Close', {
+        duration: 3000,
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom'
+      });
+      router.navigate(['/']);
+      return false;
+    }
+
+  return true;
+};
